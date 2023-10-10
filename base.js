b33c32bfad04a86b7e86174099db1472206d03d5
espmw <143532547+espmw@users.noreply.github.com>
2023-10-10 10:58:03 -0400

    yacc

--- base.js
+++ base.js
@@ -21,7 +21,8 @@ const fn_create_fx_item = (fx_list_container, title, descriptor, css_style) => {
 };
 
 const reduce_to_2decimals = (value)=>{
-  return Math.round(value*100.0)/100.0;
+  //return Math.round(value*100.0)/100.0;
+  return value.toPercision(5);
 }
 
 const g_animation_style_one = "animdir_eg_1";
